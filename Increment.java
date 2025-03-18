c9416c7e3a5a7fda45eb40101912959e05bbe477
Archishasherin <sherinsebastin2005@gmail.com>
2025-03-18 15:13:22 +0530

    FSD session2

---
+++ Increment.java
@@ -0,0 +1,12 @@
+package programs;
+
+public class Increment {
+
+	public static void main(String[] args) {
+		int a = 6;
+	    
+		 System.out.println(a++);//6(7)
+		 System.out.println(--a);//6
+		 System.out.println(++a);//6+1
+	}
+}
